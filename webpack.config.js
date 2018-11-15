62d76edcb2ecc9cde39afecff7917d5dd176e64d
mozi <mozi92@icloud.com>
2018-11-15 11:02:12 +0800

    go

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
 var path = require('path');
 
 module.exports = {
-    entry: ''
+    entry: './src/js/mDinit.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'mDrag.js'
+    },
+    module: {
+        rules: [
+            { test: /\.css$/, use: 'css-loader' },
+            { test: /\.ts$/, use: 'ts-loader' }
+        ]
+    }
 }
